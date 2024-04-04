a0a1201df5e7f18dbfdcba2f5009ef51f5a59e62
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-04-04 21:48:34 +0900

    Title: 변수 출력하기 3, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240404/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 1;
+        int b = 2; 
+        char c = 'C';
+        System.out.println(a+"->"+b+"->"+c);
+    }
+}
