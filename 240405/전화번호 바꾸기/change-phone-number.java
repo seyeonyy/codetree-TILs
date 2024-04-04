4495eb4f83104a8c150a02752a4cebab8741ce26
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-04-05 02:05:14 +0900

    Title: 전화번호 바꾸기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240405/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        String a = sc.next();
+        int xxxx = sc.nextInt();
+        int yyyy = sc.nextInt();
+
+        System.out.println(a + "-" + yyyy + "-" + xxxx);
+    }
+}
