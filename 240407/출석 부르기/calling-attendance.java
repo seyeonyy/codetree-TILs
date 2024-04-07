449719e7cfe50a384d1fe4e2de33e9ff75768a41
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-04-07 20:53:38 +0900

    Title: 출석 부르기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240407/출석 부르기/calling-attendance.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if(num == 1){
+            System.out.println("John");
+        } else if(num == 2){
+            System.out.println("Tom");
+        } else if(num == 3){
+            System.out.println("Paul");
+        } else{
+            System.out.println("Vacancy");
+        }
+    }
+}
