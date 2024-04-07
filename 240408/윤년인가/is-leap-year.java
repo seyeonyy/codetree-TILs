44bc15b28d285d633d6208bfdb4c035e34a05365
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-04-08 02:17:50 +0900

    Title: 윤년인가, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240408/윤년인가/is-leap-year.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+
+        if(y % 4 == 0){
+            if(y % 100 == 0 && y % 400 != 0){
+                System.out.println("false");
+            }else{
+                System.out.println("true");
+            }
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
