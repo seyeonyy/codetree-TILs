246223744844fed2721d600b499ad0f76918f1b8
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-04-08 02:23:50 +0900

    Title: 최댓값 구하기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240408/최댓값 구하기/maximum-value.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int max = a;
+        if(b > max){
+            max = b;
+        }
+        if(c > max){
+            max = c;
+        }
+        System.out.println(max);
+    }
+}
