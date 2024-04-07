db852b62066018fa05b7bfbca3f33cc3993c30da
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-04-07 20:37:25 +0900

    Title: 살 수 있는 물건, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240407/살 수 있는 물건/things-able-to-buy.java
+++ 240407/살 수 있는 물건/things-able-to-buy.java
@@ -5,9 +5,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        if(n == 3000){
+        if(n >= 3000){
             System.out.println("book");
-        } else if(n == 1000){
+        } else if(n >= 1000){
             System.out.println("mask");
         } else{
             System.out.println("no");
