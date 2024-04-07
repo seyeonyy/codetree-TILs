cc77e46653cb92ef68b1710ede23192da6ce489e
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-04-07 21:12:51 +0900

    Title: 최대 2번의 연산, Time: 166ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240407/최대 2번의 연산/up-to-2-calculations.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if(a % 2 == 0){
+            a /= 2;
+        }
+        if(a % 2 == 1){
+            a += 1;
+            a /=2;
+        }
+        System.out.println(a);
+    }
+}
