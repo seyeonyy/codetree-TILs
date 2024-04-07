2abfeadce3d3ffe24014765b2c8ec8fc44cbf30a
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-04-08 01:01:26 +0900

    Title: 숫자의 계절은, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240408/숫자의 계절은/season-of-num.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+
+        if(m == 12 || 1 <= m && m <= 2){
+            System.out.println("Winter");
+        } else if(m <= 5){
+            System.out.println("Spring");
+        } else if(m <= 8){
+            System.out.println("Summer");
+        } else{
+            System.out.println("Fall");
+        }
+    }
+}
