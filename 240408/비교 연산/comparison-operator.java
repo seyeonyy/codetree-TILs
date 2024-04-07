af4fd28462874c2486433547d389fa3999fccda0
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-04-08 00:03:48 +0900

    Title: 비교 연산, Time: 167ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240408/비교 연산/comparison-operator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a >= b){
+            System.out.println(1);
+        } else{
+            System.out.println(0);
+        }
+        if(a > b){
+            System.out.println(1);
+        } else{
+            System.out.println(0);
+        }
+        if(b >=a){
+            System.out.println(1);
+        } else{
+            System.out.println(0);
+        }
+        if(b > a){
+            System.out.println(1);
+        } else{
+            System.out.println(0);
+        }
+        if(a == b){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+        if(a != b){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+    }
+}
