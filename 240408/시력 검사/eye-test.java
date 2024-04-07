5c7c3aa3989cd5732e9eba86021141c1b0a8aee0
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-04-08 00:25:54 +0900

    Title: 시력 검사, Time: 136ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240408/시력 검사/eye-test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        if(a >= 1.0 && b >= 1.0){
+            System.out.println("High");
+        } else if(a >= 0.5 && b >= 0.5){
+            System.out.println("Middle");
+        } else{
+            System.out.println("Low");
+        }
+    }
+}
