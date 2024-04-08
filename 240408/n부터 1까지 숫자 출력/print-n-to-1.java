df83792619f53908c8bab322b8cc594f31c1f408
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-04-08 18:37:43 +0900

    Title: n부터 1까지 숫자 출력, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240408/n부터 1까지 숫자 출력/print-n-to-1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        while(n >= 1){
+            System.out.print(n + " ");
+            n--;
+        }
+    }
+}
