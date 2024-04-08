c42c3b43148c5485ea01e1c4c7bc6cf1ffa9f5c2
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-04-08 18:03:37 +0900

    Title: 증가시키며 출력하기 2, Time: 106ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240408/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int i = 10;
+        while(i <= 26){
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
