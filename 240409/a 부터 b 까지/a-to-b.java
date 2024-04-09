ea07e511c2273ab1a08ca911ba19b69ed92626c7
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-04-09 23:04:40 +0900

    Title: a 부터 b 까지, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240409/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+
+        for(int i = a; i <= b;){
+            System.out.print(i + " ");
+            if(i % 2 == 1){
+                i *= 2;
+            }else{
+                i += 3;
+            }
+        }
+    }
+}
