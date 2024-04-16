7ea4080de4635c8107c12861c2b55ba06af09966
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-04-16 22:11:49 +0900

    Title: 홀수만 더하기, Time: 232ms, Memory: 10MB, Status: Passed - CodeTree

--- 240416/홀수만 더하기/add-only-odd-numbers.java
+++ 240416/홀수만 더하기/add-only-odd-numbers.java
@@ -8,7 +8,7 @@ public class Main {
         int sum = 0;
         for(int i = 1; i <=n; i++){
             int num = sc.nextInt();
-            if(num % 2 == 1){
+            if(num % 2 == 1 && num % 3 == 0){
                 sum += num;
             }
         }
