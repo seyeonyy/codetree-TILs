293a19e2ecf0b6d2f8c5b314b7231726760608da
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-05-31 18:06:22 +0900

    Title: 0 나올때까지 출력, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240531/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            int n = sc.nextInt();
+            if(n == 0){
+                break;
+            }
+            System.out.println(n);
+        }
+    }
+}
