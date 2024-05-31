3e4d933528fa95f25b1062797d56346993c154db
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-05-31 15:36:40 +0900

    Title: 1까지 나누기, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240531/1까지 나누기/divide-by-1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = n;
+        int count = 0;
+        for(int i = 1; i <= n; i++){
+            num /= i;
+            count++;
+            if(num <= 1){
+                System.out.println(count);
+                break;
+            }
+        }
+    }
+}
