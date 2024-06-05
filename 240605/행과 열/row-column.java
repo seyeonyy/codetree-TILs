8937125bd51d34a59dcfd3b3d06691a9c594ad1d
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-05 14:41:03 +0900

    Title: 행과 열, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240605/행과 열/row-column.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int i = 1; i <= a; i++){
+            for(int j = 1; j <=b; j++){
+                System.out.printf((i * j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
