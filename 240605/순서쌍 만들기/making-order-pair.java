01497f640190604279ec211f6cdc8a477ead3921
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-05 14:13:54 +0900

    Title: 순서쌍 만들기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240605/순서쌍 만들기/making-order-pair.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            for(int j = n; j > 0; j--){
+                System.out.printf("(" + (n-i) +  "," +  j+ ") ");
+            }
+            System.out.println();
+        }
+    }
+}
