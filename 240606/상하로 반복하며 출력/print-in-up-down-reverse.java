307e5e64a490dd29536f38bb0eca58783ff2b6ca
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-06 00:34:30 +0900

    Title: 상하로 반복하며 출력, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240606/상하로 반복하며 출력/print-in-up-down-reverse.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(j % 2 == 0){
+                    System.out.print(1+i);
+                }
+                else{
+                    System.out.print(4-i);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
