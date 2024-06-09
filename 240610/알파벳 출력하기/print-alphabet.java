20655ae2a535c1f70d68e451388c6f1d18bdfb00
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-10 00:55:26 +0900

    Title: 알파벳 출력하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240610/알파벳 출력하기/print-alphabet.java
+++ 240610/알파벳 출력하기/print-alphabet.java
@@ -9,9 +9,10 @@ public class Main {
         for(int i = 0; i < n; i++){
             for(int j = 0; j <= i; j++){
                 System.out.print(cnt);
-                cnt++;
                 if(cnt == 'Z'){
                     cnt = 'A';
+                }else{
+                    cnt++;
                 }
             }
             System.out.println();
