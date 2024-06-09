ff238172a7fe3d9b622f6e4c1ab91771088b3063
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-09 16:47:07 +0900

    Title: 정상적인 구구단, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240609/정상적인 구구단/normal-multiple.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                System.out.print(i +" * " + j + " = " + i*j);
+                if(j < n){
+                    System.out.print(", ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
