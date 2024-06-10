519242c592e53c90317d5399e5735fff628229ff
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-10 15:52:08 +0900

    Title: 약수가 세 개인 수, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240610/약수가 세 개인 수/numbers-with-three-divisors.java
+++ 240610/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -9,7 +9,7 @@ public class Main {
 
         for(int i = start; i <= end; i++){
             int count = 0;
-            for(int j = 1; j <= start; j++){
+            for(int j = 1; j <= i; j++){
                 if(i % j == 0){
                     count++;
                 }
