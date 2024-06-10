56ad3fa682b3ad1abbd67be0a9b7920ebb95c170
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-10 15:28:52 +0900

    Title: 완전수, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

--- 240610/완전수/perfect-number.java
+++ 240610/완전수/perfect-number.java
@@ -15,8 +15,8 @@ public class Main {
                 }
             }
             if(sum == i){
-                    count++;
-                }
+                count++;
+            }
         }
         System.out.println(count);
     }
