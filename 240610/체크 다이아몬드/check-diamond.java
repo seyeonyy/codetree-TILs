31a7c412a4558660790b3e33a3cb1910247fd76b
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-10 23:13:04 +0900

    Title: 체크 다이아몬드, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240610/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n-1; j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k < i+1; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i = 0; i < n-1; i++){
+            for(int j = 0; j < i + 1; j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k < n-i-1; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
