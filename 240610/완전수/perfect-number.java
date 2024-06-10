7ff7ea2373224197fb6e693a4f047cbeb27ccecc
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-10 15:28:14 +0900

    Title: 완전수, Time: 235ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240610/완전수/perfect-number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int count = 0;
+
+        for(int i = start; i <= end; i++){
+            int sum = 0;
+            for(int j = 1; j <= i / 2; j++){
+                if(i % j == 0){
+                    sum += j;
+                }
+            }
+            if(sum == i){
+                    count++;
+                }
+        }
+        System.out.println(count);
+    }
+}
