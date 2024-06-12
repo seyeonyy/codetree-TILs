9ce705412bacd2dbceded35e3ab1f595427dfa5d
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-12 16:13:19 +0900

    Title: 1-9 개수 세기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240612/1-9 개수 세기/count-one-to-nine.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] arr2 = new int[9];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n; i++){
+            arr2[arr[i]-1]++;
+        }
+        for(int i = 0; i < 9; i++){
+            System.out.println(arr2[i]);
+        }
+    }
+}
