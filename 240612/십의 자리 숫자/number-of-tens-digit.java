9ffb98cfc5fd0251bebc96408807fa5a80004bc8
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-12 16:56:41 +0900

    Title: 십의 자리 숫자, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240612/십의 자리 숫자/number-of-tens-digit.java
+++ 240612/십의 자리 숫자/number-of-tens-digit.java
@@ -3,10 +3,10 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[100];
-        int[] arr2 = new int[100];
+        int[] arr = new int[99];
+        int[] arr2 = new int[10];
 
-        for(int i = 0; i < 100; i++){
+        for(int i = 0; i < 99; i++){
             arr[i] = sc.nextInt();
             if(arr[i] == 0){
                 break;
