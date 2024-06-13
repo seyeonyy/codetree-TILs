1d3ff4c81bd20564e4764d1c07a160783c157e31
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-13 23:56:45 +0900

    Title: 가장 큰 수 고르기, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240613/가장 큰 수 고르기/picking-biggest-number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        
+        for(int i = 0; i < 10; i++){
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for(int i = 1; i < 10; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
