9de16a566829c49772383e965e8b21003ede0fe1
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-13 16:50:00 +0900

    Title: 배열 놀이, Time: 203ms, Memory: 11MB, Status: Passed - CodeTree

--- 240613/배열 놀이/play-with-array.java
+++ 240613/배열 놀이/play-with-array.java
@@ -11,8 +11,7 @@ public class Main {
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
-
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < 100; i++){
             if(cnt == 0){
                 break;
             }
