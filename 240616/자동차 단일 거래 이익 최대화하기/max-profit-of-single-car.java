307e71317cd066820f85e185025db027e195a616
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-16 01:17:29 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 247ms, Memory: 13MB, Status: Passed - CodeTree

--- 240616/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
+++ 240616/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -5,7 +5,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        int idx = 0;
 
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
