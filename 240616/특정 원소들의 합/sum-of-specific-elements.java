ca59e754ebff6be1da7e9391a4b11723269f171d
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-16 17:43:14 +0900

    Title: 특정 원소들의 합, Time: 175ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240616/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[4][4];
+        int sum = 0;
+
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < i+1; j++){
+                sum += arr[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
