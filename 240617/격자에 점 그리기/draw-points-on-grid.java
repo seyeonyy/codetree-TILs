7597397171fd07ae629654c3b6a0c0e36a2796c2
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-17 01:19:30 +0900

    Title: 격자에 점 그리기, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240617/격자에 점 그리기/draw-points-on-grid.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n+1][n+1];
+        int num = 1;
+        for(int i = 0; i < m; i++){
+            int r = sc.nextInt();
+            int c = sc.nextInt();
+            arr[r][c] = num++;
+        }
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < n+1; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
