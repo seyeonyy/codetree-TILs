1c3c8a1a4368b2cbc70889c2904b80776c22eeac
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-16 18:43:23 +0900

    Title: 숫자 직사각형, Time: 203ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240616/숫자 직사각형/rectangle-of-numbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+
+        int num = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                arr[i][j] = num;
+                num += 1;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
