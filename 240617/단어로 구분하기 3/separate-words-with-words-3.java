7dc181915e769dec22e1191260b4bfe8be66c2f6
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-17 14:55:41 +0900

    Title: 단어로 구분하기 3, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240617/단어로 구분하기 3/separate-words-with-words-3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String[] str = new String[10];
+        for(int i = 0; i < 10; i++){
+            str[i] = sc.next();
+        }
+        for(int i = 9; i >= 0; i--){
+            System.out.println(str[i]);
+        }
+    }
+}
