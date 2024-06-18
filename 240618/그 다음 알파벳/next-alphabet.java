3b8a27919fc44d122031932bc58617907cc15b08
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-18 22:11:08 +0900

    Title: 그 다음 알파벳, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240618/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        if(c == 'z'){
+            System.out.print("a");
+        }else{
+            int n = (int)c;
+            System.out.print((char)(n+1));
+        }
+    }
+}
