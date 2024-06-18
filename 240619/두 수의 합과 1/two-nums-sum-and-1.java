17631a2e0b04aa1d76272ad82ddad034338c93e9
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-19 00:05:03 +0900

    Title: 두 수의 합과 1, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240619/두 수의 합과 1/two-nums-sum-and-1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int sum = n1+n2;
+        String s = Integer.toString(sum);
+        int count = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '1'){
+                count++;
+            }
+        }
+        System.out.print(count);
+    }
+}
