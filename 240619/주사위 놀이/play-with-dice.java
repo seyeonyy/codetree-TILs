feb28386e15e8e28d08dfc9dd897efd7a7925a70
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-19 23:13:43 +0900

    Title: 주사위 놀이, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240619/주사위 놀이/play-with-dice.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr2 = new int[7];
+
+        for(int i = 0; i < 10; i++){
+            int n = sc.nextInt();
+            arr2[n-1]++;
+        }
+        for(int i = 1; i <= 6; i++){
+            System.out.println(i + " - " + arr2[i-1]);
+        }
+    }
+}
