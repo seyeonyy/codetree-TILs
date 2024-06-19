7b6d186fe7df0f5f6a1472ea5f409167034f6701
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-19 18:28:54 +0900

    Title: 특이한 출력, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240619/특이한 출력/special-print.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                System.out.print("(" + i + ", " + j + ") ");
+                if((i + j) % 4 == 0){
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
