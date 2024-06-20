4a4150fd181a8da5539dba010973efc0d5f17566
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-21 02:19:21 +0900

    Title: 문자열 계속 지우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240621/문자열 계속 지우기/keep-removing-string.java
+++ 240621/문자열 계속 지우기/keep-removing-string.java
@@ -18,57 +18,11 @@ public class Main {
                 if(count == lenB){
                     A = A.substring(0, i) + A.substring(i+lenB);
                     lenA = A.length();
-                    i = 0;
+                    i = -1;
                     count = 1;
                 }
             }
         }
-        System.out.print(A);
-        
-        /*for(int i = 0; i < lenA; i++){
-            if(A.charAt(i) == B.charAt(0)){
-                for(int j = 1; j < lenB; j++){
-                    if(arr[i+j] == arr[j]){
-                        count++;
-                        if(count == lenB){
-                            lenA = A.substring(i);
-                            break;
-                        }
-                    }else{
-                        count = 1;
-                    }
-                }
-            }
-            if(count == lenB){
-                lenA = 
-            }
-        }*/
-        /*while(true){
-            int idx = 0; 
-            for(int i = 0; i < lenA; i++){
-                int count = 0;
-                if(arr[i] == B.charAt(0)){
-                    for(int j = 1; j < lenB; j++){
-                        if(i+j > lenA){
-                            break;
-                        }
-                        if(arr[i+j] == B.charAt(j)){
-                            count++;
-                        }else{
-                            break;
-                        }
-                    }
-                    System.out.print(count);
-                    idx = i;
-                }
-            }
-            System.out.print(idx);*/
-            /*if(count == lenB){
-                idx -= lenB + 1;
-                int idx2 = idx+lenB;
-                A = A.substring(0,idx) + A.substring(idx2);
-                System.out.println(A);
-            }*/
-            //break;           
+        System.out.print(A);       
     }
 }
