7510a1071aac6ec9a5aa18831857956a7493d030
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-06-20 21:41:58 +0900

    Title: 지그재그로 숫자 채우기, Time: 193ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240620/지그재그로 숫자 채우기/zigzag-numbering.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+        int num = 0;
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < m; i++){
+            if(x == n-1){
+                for(int j = 0; j < n; j++){
+                    arr[x][y+i] = num++;
+                    x--;
+                    if(x == -1){
+                        x = 0;
+                    }
+                }
+            }else{
+                for(int j = 0; j < n; j++){
+                    arr[x][y+i] = num++;
+                    x++;
+                    if(x == n){
+                        x = n-1;
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
