39f1d4bd6ce9e4aeb81cea191ddec65947ae8b27
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-07-12 23:44:38 +0900

    Title: 그 계절, 그 날, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240712/그 계절, 그 날/that-season-that-day.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int Y = sc.nextInt();
+        int M = sc.nextInt();
+        int D = sc.nextInt();
+        if(season(Y,M,D).equals("xx")){
+            System.out.println(-1);
+        }else{
+            System.out.println(season(Y,M,D));
+        }
+    }
+    public static boolean year(int Y){
+        if(Y % 4 == 0 && Y % 100 != 0 || Y % 4 == 0 && Y % 100 == 0 && Y % 400 == 0){
+            return true;
+        }
+        return false;
+    }
+    public static int day(int Y, int M){
+        if(year(Y) && M == 2){
+            return 29;
+        }else if(M == 3 || M == 5 || M == 7 || M == 8 || M == 10 || M == 1 || M == 12){
+            return 31;
+        }
+        return 30;
+    }
+    public static String season(int Y, int M, int D){
+        if(D <= day(Y,M)){
+            if(3 <= M && M <= 5){
+                return "Spring";
+            }else if(6 <= M && M <= 8){
+                return "Summer";
+            }else if(9 <= M && M <= 11){
+                return "Fall";
+            }else{
+                return "Winter";
+            }
+        }
+        return "xx";
+    }
+}
