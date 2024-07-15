d36338dcf1fd96f80bbe22a88f103ac2decdd381
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-07-15 18:29:13 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 205ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240715/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printStar(n);
+    }
+    public static void printStar(int n){
+        if(n == 0){
+            return;
+        }
+        for(int i = 0; i < n; i++){
+            System.out.print("* ");
+        }
+        System.out.println();
+        printStar(n-1);
+        for(int i = 0; i < n; i++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
