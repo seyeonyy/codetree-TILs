146f0e0b544bb0b607822539b7c04dd14dff0089
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-07-21 03:57:41 +0900

    Title: k번째로 신기한 문자열, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240721/k번째로 신기한 문자열/kth-special-string.java
+++ 240721/k번째로 신기한 문자열/kth-special-string.java
@@ -7,36 +7,20 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
         String T = sc.next();
-        // System.out.println(String.format("T: %s", T));
         String[] str = new String[n];
         for(int i = 0; i < n; i++){
-            try {
-                str[i] = sc.next();
-            } catch (Exception ex) {
-                System.out.println(String.format("Current i: %d, n: %d, k: %d, T: %s", i, n, k, T));
-            }
-            // System.out.println(String.format("str[%d]: %s", i, str[i]));
+            str[i] = sc.next();
         }
         Arrays.sort(str);
         String[] str2 = new String[n];
+        int idx = 0;
         for(int i = 0; i < n; i++){
-            if(str[i].substring(0,T.length()).equals(T)){
-                str2[i] = str[i];
+            if(str[i].length() >= T.length()){
+                if(str[i].substring(0,T.length()).equals(T)){
+                    str2[idx++] = str[i];
+                }
             }
         }
-        int count = 0;
-        for(int i = 0; i < n; i++){
-            if(str2[i] != null){
-                count++;
-            }
-        }
-        int index = 0;
-        String[] str3 = new String[count];
-        for(int i = 0; i < n; i++){
-            if(str2[i] != null){
-                str3[index++] = str2[i];
-            }
-        }
-        System.out.println(str3[k-1]);
+        System.out.println(str2[k-1]);
     }
 }
