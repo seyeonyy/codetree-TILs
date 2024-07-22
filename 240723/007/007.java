101ab6405484421a2568d7e294faa2eebea73a57
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-07-23 00:59:00 +0900

    Title: 007, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240723/007/007.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Print{
+    String secretCode;
+    char meetingPoint;
+    int time;
+    Print(String secretCode, char meetingPoint, int time){
+        this.secretCode = secretCode;
+        this.meetingPoint = meetingPoint;
+        this.time = time;
+        System.out.println("secret code : " + this.secretCode);
+        System.out.println("meeting point : " + this.meetingPoint);
+        System.out.println("time : " + this.time);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String secretCode = sc.next();
+        char meetingPoint = sc.next().charAt(0);
+        int time = sc.nextInt();
+        Print p = new Print(secretCode, meetingPoint, time);
+    }
+}
