dcda9038e0b2e980d9a924b226bca7fb76173a6a
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-08-05 21:26:26 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 203ms, Memory: 43MB, Status: Wrong Answer - CodeTree

--- 240805/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 240805/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static final int OFFSET = 1000;
     public static int[][] square1 = new int[OFFSET*2+1][OFFSET*2+1];
-    //public static int[][] square2 = new int[OFFSET*2+1][OFFSET*2+1];
+    public static int[][] square2 = new int[OFFSET*2+1][OFFSET*2+1];
     public static int x1;
     public static int y1;
     public static int x2;
@@ -16,7 +16,7 @@ public class Main {
         y2 = sc.nextInt();
         for(int j = OFFSET+y1; j < OFFSET+y2; j++){
             for(int k = OFFSET+x1; k < OFFSET+x2; k++){
-                square1[j][k] += 1;
+                square1[j][k] = 1;
             }
         }
         x1 = sc.nextInt();
@@ -25,33 +25,35 @@ public class Main {
         y2 = sc.nextInt();
         for(int j = OFFSET+y1; j < OFFSET+y2; j++){
             for(int k = OFFSET+x1; k < OFFSET+x2; k++){
-                square1[j][k] += 2;
+                if(square1[j][k] == 1){
+                    square2[j][k] = 2;
+                    continue;
+                }
+                square2[j][k] = 1;
             }
         }
-        /*for(int i = OFFSET-1; i < OFFSET+11; i++){
-            for(int j = OFFSET-1; j < OFFSET+11; j++){
-                System.out.println(i + " " + j + " " +square1[i][j]);
-            }
-        }*/
         int cnt1 = 0;
+        int cnt3 = 0;
         int cnt2 = 0;
+        int cnt4 = 0;
         for(int i = 0; i < OFFSET*2+1; i++){
             for(int j = 0; j < OFFSET*2+1; j++){
+                if(square2[i][j] >= 1){
+                    cnt2++;
+                }
+                if(square2[i][j] == 2){
+                    cnt3++;
+                }
                 if(square1[i][j] == 1){
                     cnt1++;
                 }
-                if(square1[i][j] == 3){
-                    cnt2++;
-                }
             }
         }
-        if(cnt1 < cnt2){
-            cnt1 = cnt1;
-        }else if(cnt1 > cnt2){
-            cnt1 += cnt2;
+        if(cnt2 > cnt1){
+            System.out.println(cnt1);
         }else{
-            cnt1-= cnt2;
+            cnt4 = cnt1 - cnt3;
+            System.out.println(cnt4);
         }
-        System.out.println(cnt1);
     }
 }
