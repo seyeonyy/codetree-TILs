55dd21537716ee44e3e67e9d602c79bc62be050d
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-08-05 23:33:59 +0900

    Title: 연속되는 수 3, Time: 183ms, Memory: 11MB, Status: Passed - CodeTree

--- 240805/연속되는 수 3/continuous-number3.java
+++ 240805/연속되는 수 3/continuous-number3.java
@@ -12,7 +12,7 @@ public class Main {
         int cnt = 1;
         int m = 0;
         for(int i = 0; i < N; i++){
-            if(i >= 1 && (arr[i] >= 0 && arr[i-1] >= 0) || arr[i] < 0 && arr[i-1] < 0){
+            if(i >= 1 && ((arr[i] >= 0 && arr[i-1] >= 0) || (arr[i] < 0 && arr[i-1] < 0))){
                 cnt++;
             }else{
                 cnt = 1;
