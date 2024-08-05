d4252f1abfbf723ae14902cf939454ae20108f40
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-08-05 22:44:08 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 150ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240805/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 240805/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static final int OFFSET = 1000;
     public static int[][] square1 = new int[OFFSET*2+1][OFFSET*2+1];
-    public static int[][] square2 = new int[OFFSET*2+1][OFFSET*2+1];
+    //public static int[][] square2 = new int[OFFSET*2+1][OFFSET*2+1];
     public static int x1;
     public static int y1;
     public static int x2;
@@ -10,52 +10,34 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        x1 = sc.nextInt();
-        y1 = sc.nextInt();
-        x2 = sc.nextInt();
-        y2 = sc.nextInt();
-        for(int j = OFFSET+y1; j < OFFSET+y2; j++){
-            for(int k = OFFSET+x1; k < OFFSET+x2; k++){
-                square1[j][k] = 1;
-            }
-        }
-        x1 = sc.nextInt();
-        y1 = sc.nextInt();
-        x2 = sc.nextInt();
-        y2 = sc.nextInt();
-        for(int j = OFFSET+y1; j < OFFSET+y2; j++){
-            for(int k = OFFSET+x1; k < OFFSET+x2; k++){
-                if(square1[j][k] == 1){
-                    square2[j][k] = 2;
-                    square1[j][k] = 0;
-                    continue;
+        for(int i = 0; i < 2; i++){
+            x1 = sc.nextInt();
+            y1 = sc.nextInt();
+            x2 = sc.nextInt();
+            y2 = sc.nextInt();
+            for(int j = OFFSET+x1; j < OFFSET+x2; j++){
+                for(int k = OFFSET+y1; k < OFFSET+y2; k++){
+                    square1[j][k] = i+1;
                 }
-                square2[j][k] = 1;
             }
         }
-        int cnt1 = 0;
-        int cnt3 = 0;
-        int cnt2 = 0;
+        int minX = 0;
+        int maxX = 0;
+        int minY = 0;
+        int maxY = 0;
+        boolean first = true;
         for(int i = 0; i < OFFSET*2+1; i++){
             for(int j = 0; j < OFFSET*2+1; j++){
-                if(square2[i][j] >= 1){
-                    cnt2++;
-                }
-                if(square2[i][j] == 2){
-                    cnt3++;
-                }
-                if(square1[i][j] == 1){
-                    cnt1++;
+                if(square1[i][j] == 1 && first){
+                    minX = i;
+                    minY = j;
+                    first = false;
+                }else if(square1[i][j] == 1){
+                    maxX = i;
+                    maxY = j;
                 }
             }
         }
-        if(cnt1 == 0){
-            System.out.println(cnt1);
-        }else if(cnt2+cnt3 > cnt1){
-            cnt1 += cnt3;
-            System.out.println(cnt1);
-        }else{
-            System.out.println(cnt1);
-        }
+        System.out.println((maxX-minX+1)*(maxY-minY+1));
     }
 }
