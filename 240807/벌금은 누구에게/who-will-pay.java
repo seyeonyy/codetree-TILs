7763ac45665bbeca6398c227612c0634aa093eff
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-08-07 21:31:53 +0900

    Title: 벌금은 누구에게, Time: 272ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 240807/벌금은 누구에게/who-will-pay.java
+++ 240807/벌금은 누구에게/who-will-pay.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 public class Main {
     public static final int MAX_N = 10000;
-    public static final int MAX_S = 100;
     public static int[] arr = new int[MAX_N+1];
-    public static int[] num = new int[MAX_S+1];
+    public static int[] num = new int[MAX_N+1];
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
@@ -14,14 +13,11 @@ public class Main {
             arr[i] = sc.nextInt();
         }
         int idx = -1;
-        for(int i = 1; i <= n; i++){
-            int cnt = 0;
-            for(int j = 0; j < m; j++){
-                if(arr[j] == i){
-                    cnt++;
-                }
-            }
-            if(cnt >= k){
+        for(int i = 0; i < m; i++){
+            num[arr[i]]++;
+        }
+        for(int i = 1; i < m; i++){
+            if(num[i] >= k){
                 idx = i;
                 break;
             }
