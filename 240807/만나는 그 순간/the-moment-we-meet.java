8f8c0d629af40d128cf5dfbecdd3431304bec11f
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-08-07 18:13:11 +0900

    Title: 만나는 그 순간, Time: 262ms, Memory: 29MB, Status: Passed - CodeTree

--- 240807/만나는 그 순간/the-moment-we-meet.java
+++ 240807/만나는 그 순간/the-moment-we-meet.java
@@ -4,8 +4,9 @@ public class Main {
     public static int m;
     public static char d;
     public static int t;
-    public static int[] arr1 = new int[1002];
-    public static int[] arr2 = new int[1002];
+    public static final int MAX_T = 1000 * 1000;
+    public static int[] arr1 = new int[MAX_T+1];
+    public static int[] arr2 = new int[MAX_T+1];
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
