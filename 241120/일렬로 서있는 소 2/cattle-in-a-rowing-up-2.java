33f87af1f1b30370bd765ab2efda7598c9577cfe
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2024-11-20 19:03:17 +0900

    Title: 일렬로 서있는 소 2, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241120/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = sc.nextInt();
+        }
+        int cnt = 0;
+        for(int i = 0; i < N; i++){
+            for(int j = i+1; j < N; j++){
+                for(int k = j+1; k < N; k++){
+                    if(arr[i] <= arr[j] && arr[j] <= arr[k]){
+                        cnt++;
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
