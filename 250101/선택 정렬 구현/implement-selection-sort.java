537b0da906c397193f2557076e2350c4b40ca4e6
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2025-01-01 23:47:36 +0900

    Title: 선택 정렬 구현, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250101/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(arr[i] > arr[j]){
+                    int tmp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = tmp;
+                }
+            }
+        }
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
