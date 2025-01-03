8e7bf26b2e9b81ac0c44e4bac090db9b33a3aa82
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2025-01-03 22:13:25 +0900

    Title: 기수 정렬 구현, Time: 1118ms, Memory: 35MB, Status: Passed - CodeTree

--- 250103/기수 정렬 구현/implement-radix-sort.java
+++ 250103/기수 정렬 구현/implement-radix-sort.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public class Main {
     public static int[] arr = new int[100000];
     public static int n;
-    public static ArrayList<Integer>[] newArr = new ArrayList[10];
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
@@ -11,21 +10,19 @@ public class Main {
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
-        for(int i = 0; i < newArr.length; i++){
-            newArr[i] = new ArrayList<>();
-        }
         radixSort();
         for(int i = 0; i < n; i++){
             System.out.print(arr[i] + " ");
         }
     }
     public static void radixSort(){
-        for(int pos = 0; pos <= 5; pos++){
+        for(int pos = 0; pos < 6; pos++){
+            ArrayList<Integer>[] newArr = new ArrayList[10];
             for(int i = 0; i < newArr.length; i++){
-                newArr[i].clear();
+                newArr[i] = new ArrayList<>();
             }
             for(int i = 0; i < n; i++){
-                int digit = (arr[i] % ((int)Math.pow(10, pos))) % 10;
+                int digit = (arr[i] / (int)Math.pow(10, pos)) % 10;
                 newArr[digit].add(arr[i]);
             }
             int index = 0;
