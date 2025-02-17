3f9912ef02604f2547ceba48ae29d6d79838a09f
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2025-02-17 22:56:53 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 147ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 250217/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class Main {
+    public static int x1;
+    public static int y1;
+    public static int x2;
+    public static int y2;
+    public static int OFFSET = 1000;
+    public static int[][] square = new int[2001][2001];
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int area1 = 0;
+        int area3 = 0;
+        for(int i = 0; i < 2; i++){
+            x1 = sc.nextInt();
+            y1 = sc.nextInt();
+            x2 = sc.nextInt();
+            y2 = sc.nextInt();
+            x1 += OFFSET;
+            y1 += OFFSET;
+            x2 += OFFSET;
+            y2 += OFFSET;
+            if(i == 0){
+                for(int j = x1; j<= x2-1; j++){
+                    for(int k = y1; k <= y2-1; k++){
+                        square[j][k] = 1;
+                        area1++;
+                    }
+                }
+            }else{
+                for(int j = x1; j<= x2-1; j++){
+                    for(int k = y1; k <= y2-1; k++){
+                        if(square[j][k] == 0){
+                            square[j][k] = 2;
+                        }else{
+                            square[j][k] = 3;
+                            area3++;
+                        }
+                    }
+                }
+            }
+        }
+        if(area1 > area3){
+            System.out.println(area1);
+        }else{
+            System.out.println(0);
+        }
+    }
+}
