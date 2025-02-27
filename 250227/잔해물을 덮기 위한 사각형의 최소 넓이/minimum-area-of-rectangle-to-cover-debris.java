9dd63c78f22a7e2d7d54085b8e366286cba8d24a
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2025-02-27 22:58:46 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 174ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 250227/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[2000][2000];
+        int ax1 = sc.nextInt()+1000;
+        int ay1 = sc.nextInt()+1000;
+        int ax2 = sc.nextInt()+1000;
+        int ay2 = sc.nextInt()+1000;
+        int bx1 = sc.nextInt()+1000;
+        int by1 = sc.nextInt()+1000;
+        int bx2 = sc.nextInt()+1000;
+        int by2 = sc.nextInt()+1000;
+        for(int i = ax1; i < ax2; i++){
+            for(int j = ay1; j < ay2; j++){
+                arr[i][j] = 1;
+            }
+        }
+        for(int i = bx1; i < bx2; i++){
+            for(int j = by1; j < by2; j++){
+                if(arr[i][j] == 1){
+                    arr[i][j] = 2;
+                }else{
+                    arr[i][j] = 3;
+                }
+            }
+        }
+        int cnt = 0;
+        int cnt2 = 0;
+        for(int i = 0; i < 2000; i++){
+            for(int j = 0; j < 2000; j++){
+                if(arr[i][j] == 1){
+                    cnt++;
+                }
+                if(arr[i][j] == 2){
+                    cnt2++;
+                }
+            }
+        }
+        if(cnt2 > cnt){
+            System.out.println(cnt);
+        }else{
+            System.out.println(cnt+cnt2);
+        }
+    }
+}
