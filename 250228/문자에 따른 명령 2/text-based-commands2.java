f0099265ffdc741b5cbf68206ad682339ef416c8
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2025-02-28 20:17:20 +0900

    Title: 문자에 따른 명령 2, Time: 167ms, Memory: 11MB, Status: Runtime Error - CodeTree

---
+++ 250228/문자에 따른 명령 2/text-based-commands2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int[] dx = new int[]{1, 0, -1, 0};
+        int[] dy = new int[]{0, -1, 0, 1};
+        int x = 0;
+        int y = 0;
+        int dirNum = 3;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == 'L'){
+                dirNum = (dirNum-1)%4;
+            }else if(str.charAt(i) == 'R'){
+                dirNum = (dirNum+1)%4;
+            }else{
+                x += dx[dirNum];
+                y += dy[dirNum];
+            }
+        }
+        System.out.println(x + " " + y);
+    }
+}
