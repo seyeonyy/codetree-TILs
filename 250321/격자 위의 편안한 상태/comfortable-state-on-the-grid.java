f5aabaedf4825c211b61bbbe6a791809a919d502
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2025-03-21 17:21:29 +0900

    Title: 격자 위의 편안한 상태, Time: 135ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250321/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] arr = new int[201][201];
+        for(int i = 0; i < M; i++){
+            int r = sc.nextInt()+100;
+            int c = sc.nextInt()+100;
+            arr[r][c] = 1;
+            int cnt = 0;
+            if(arr[r-1][c] == 1){
+                cnt++;
+            }
+            if(arr[r][c-1] == 1){
+                cnt++;
+            }
+            if(arr[r][c+1] == 1){
+                cnt++;
+            }
+            if(arr[r+1][c] == 1){
+                cnt++;
+            }
+            if(cnt == 3){
+                System.out.println(1);
+            }else{
+                System.out.println(0);
+            }
+        }
+    }
+}
