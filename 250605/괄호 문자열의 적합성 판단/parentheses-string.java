37b70b3d4e31ab393244841fd5f5f12f81ce796c
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2025-06-05 17:11:24 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 250605/괄호 문자열의 적합성 판단/parentheses-string.java
+++ 250605/괄호 문자열의 적합성 판단/parentheses-string.java
@@ -7,38 +7,20 @@ public class Main {
         String str = sc.next();
         //int cnt = 0;
         for(int i = 0; i < str.length(); i++){
-            if(str.charAt(i) == '('){
-                s.push(str.charAt(i));
-            }else if(str.charAt(i) == ')'){
+            if(str.charAt(i) == ')'){
                 if(s.isEmpty()){
                     continue;
-                }s.pop();
+                }
+                s.pop();
+            }else if(str.charAt(i) == '('){
+                s.push(str.charAt(i));
             }
-            
-            // s.push(str.charAt(i));
-            // if(s.peek() == '('){
-            //     cnt++;
-            //     System.out.println(cnt);
-            // }else{
-            //     cnt--;
-            //     System.out.println(cnt);
-            // }
         }
-        // int cnt = 0;
-        // for(int i = 0; i < str.length(); i++){
-        //     if(s.peek() == '('){
-        //         cnt++;
-        //         System.out.println(cnt);
-        //     }else{
-        //         cnt--;
-        //         System.out.println(cnt);
-        //     }
-        //     s.pop();
-        // }
-        if(s.isEmpty()){
+        if(str.charAt(0) != ')' && s.isEmpty()){
             System.out.println("Yes");
         }else{
             System.out.println("No");
         }
+
     }
 }
