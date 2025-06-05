af1dc69451d37668f8b55474b037a44d7d640edc
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2025-06-05 16:28:06 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250605/괄호 문자열의 적합성 판단/parentheses-string.java
+++ 250605/괄호 문자열의 적합성 판단/parentheses-string.java
@@ -5,19 +5,37 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Stack<Character> s = new Stack<>();
         String str = sc.next();
+        //int cnt = 0;
         for(int i = 0; i < str.length(); i++){
-            s.push(str.charAt(i));
-        }
-        int cnt = 0;
-        for(int i = 0; i < str.length(); i++){
-            if(s.peek() == '('){
-                cnt++;
-            }else{
-                cnt--;
+            if(str.charAt(i) == '('){
+                s.push(str.charAt(i));
+            }else if(str.charAt(i) == ')'){
+                if(s.isEmpty()){
+                    continue;
+                }s.pop();
             }
-            s.pop();
+            
+            // s.push(str.charAt(i));
+            // if(s.peek() == '('){
+            //     cnt++;
+            //     System.out.println(cnt);
+            // }else{
+            //     cnt--;
+            //     System.out.println(cnt);
+            // }
         }
-        if(cnt == 0){
+        // int cnt = 0;
+        // for(int i = 0; i < str.length(); i++){
+        //     if(s.peek() == '('){
+        //         cnt++;
+        //         System.out.println(cnt);
+        //     }else{
+        //         cnt--;
+        //         System.out.println(cnt);
+        //     }
+        //     s.pop();
+        // }
+        if(s.isEmpty()){
             System.out.println("Yes");
         }else{
             System.out.println("No");
