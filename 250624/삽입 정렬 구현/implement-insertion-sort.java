54a37131a9ac885fdceff337d8bf7763f748685a
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2025-06-24 16:10:33 +0900

    Title: 삽입 정렬 구현, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250624/삽입 정렬 구현/implement-insertion-sort.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 1; i < n; i++){
+            int j = i - 1;
+            int key = arr[i];
+            while(j >= 0 && key < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
