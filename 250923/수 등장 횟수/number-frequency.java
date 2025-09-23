bf43a2b54dcf29aa516d8f03258167e33d81d7cc
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2025-09-23 23:24:17 +0900

    Title: 수 등장 횟수, Time: 1503ms, Memory: 58MB, Status: Wrong Answer - Codetree

--- 250923/수 등장 횟수/number-frequency.java
+++ 250923/수 등장 횟수/number-frequency.java
@@ -19,6 +19,8 @@ public class Main {
             for(int j = 0; j < n; j++){
                 if(numToIndex.get(j) == num){
                     cnt++;
+                }else{
+                    break;
                 }
             }
             System.out.print(cnt + " ");
