2e681ef1aa0d2a2b23fd5a7861c534bc74ddeb75
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2026-01-03 00:00:32 +0900

    Title: 두 선분, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260103/두 선분/two-lines.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int x3 = sc.nextInt();
+        int x4 = sc.nextInt();
+        // Please write your code here.
+        if(x1 <= x3 && x3 <= x2){
+            System.out.println("intersecting");
+        }else if(x3 <= x1 && x1 <= x4){
+            System.out.println("intersecting");
+        }else{
+            System.out.println("nonintersecting");
+        }
+    }
+}
