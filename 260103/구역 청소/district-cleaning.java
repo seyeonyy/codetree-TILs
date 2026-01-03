c906752a7decb02e949bb1074f48926becfa0c04
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2026-01-03 11:01:15 +0900

    Title: 구역 청소, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260103/구역 청소/district-cleaning.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        // Please write your code here.
+        int min = 100;
+        int max = 0;
+        if(a <= c){
+            min = a;
+        }else if(c <= a){
+            min = c;
+        }
+        if(b <= d){
+            max = d; 
+        }else if(d <= b){
+            max = b;
+        }
+        System.out.println(max-min);
+    }
+}
