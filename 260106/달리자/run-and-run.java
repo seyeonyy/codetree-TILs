e26886ab50f832695bfabf4e85804cff181be59d
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2026-01-06 11:39:46 +0900

    Title: 달리자, Time: 146ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260106/달리자/run-and-run.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            b[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            int d = 1;
+            for(int j = i+1; j < i+2; j++){
+                if(a[i] > b[i]){
+                    sum += d * (a[i] - b[i]);
+                    a[i+1] += a[i] - b[i];
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
