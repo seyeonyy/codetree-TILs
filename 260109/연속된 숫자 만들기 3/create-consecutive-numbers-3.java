b5670b379764f3672d78f131dae3e97a6841cf2b
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2026-01-09 11:13:07 +0900

    Title: 연속된 숫자 만들기 3, Time: 617ms, Memory: 10MB, Status: Passed - Codetree

--- 260109/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
+++ 260109/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
@@ -9,16 +9,7 @@ public class Main {
         int result = 0;
         if(b - a == 1 && c - b == 1){
             result = 0;
-        }else if(b - a == 1){
-            int tmp = 0;
-            while(!(b - a == 1 && c - b == 1)){
-                a = b+1;
-                tmp = a;
-                a = b;
-                b = tmp;
-                result++;
-            }
-        }else{
+        }else if(b - a > c - b){
             int tmp = 0;
             while(!(b - a == 1 && c - b == 1)){
                 c = b-1;
@@ -27,7 +18,36 @@ public class Main {
                 b = tmp;
                 result++;
             }
+        }else{
+            int tmp = 0;
+            while(!(b - a == 1 && c - b == 1)){
+                a = b+1;
+                tmp = a;
+                a = b;
+                b = tmp;
+                result++;
+            }
         }
+        // }else if(b - a == 1){
+        //     int tmp = 0;
+        //     while(!(b - a == 1 && c - b == 1)){
+        //         a = b+1;
+        //         tmp = a;
+        //         a = b;
+        //         b = tmp;
+        //         result++;
+        //     }
+        // }else if(b )
+        // }else{
+        //     int tmp = 0;
+        //     while(!(b - a == 1 && c - b == 1)){
+        //         c = b-1;
+        //         tmp = c;
+        //         c = b;
+        //         b = tmp;
+        //         result++;
+        //     }
+        // }
         System.out.println(result);
     }
 }
