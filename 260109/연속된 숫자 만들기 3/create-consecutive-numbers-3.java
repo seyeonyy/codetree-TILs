68e7e565d135735655a17b0640f4759472e1fe53
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2026-01-09 11:06:54 +0900

    Title: 연속된 숫자 만들기 3, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260109/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // Please write your code here.
+        int result = 0;
+        if(b - a == 1 && c - b == 1){
+            result = 0;
+        }else if(b - a == 1){
+            int tmp = 0;
+            while(!(b - a == 1 && c - b == 1)){
+                a = b+1;
+                tmp = a;
+                a = b;
+                b = tmp;
+                result++;
+            }
+        }else{
+            int tmp = 0;
+            while(!(b - a == 1 && c - b == 1)){
+                c = b-1;
+                tmp = c;
+                c = b;
+                b = tmp;
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
