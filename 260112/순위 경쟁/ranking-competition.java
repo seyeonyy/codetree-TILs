b1e0be0851264d28c8a2b5bf22902eca6f33eb5d
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2026-01-12 23:29:48 +0900

    Title: 순위 경쟁, Time: 114ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260112/순위 경쟁/ranking-competition.java
+++ 260112/순위 경쟁/ranking-competition.java
@@ -26,18 +26,22 @@ public class Main {
         for(int i = 1; i <= n; i++){
             if(a[i-1] == b[i-1] && b[i-1] == c[i-1]){
                 if(a[i] != b[i] || b[i] != c[i]){
+                    //System.out.println(1);
                     cnt++;
                 }
             }else if(a[i-1] > b[i-1] && b[i-1] >= c[i-1] || a[i-1] > c[i-1] && c[i-1] >= b[i-1]){
-                if(b[i] > c[i] && c[i] >= a[i] || b[i] > a[i] && a[i] >= c[i] || c[i] > a[i] && a[i] >= b[i] || c[i] > b[i] && b[i] >= a[i]){
+                if(b[i] > c[i] && c[i] >= a[i] || b[i] > a[i] && a[i] >= c[i] || c[i] > a[i] && a[i] >= b[i] || c[i] > b[i] && b[i] >= a[i] || a[i] == b[i] && b[i] == c[i]){
+                    //System.out.println(2);
                     cnt++;
                 }
             }else if(b[i-1] > c[i-1] && c[i-1] >= a[i-1] || b[i-1] > a[i-1] && a[i-1] >= c[i-1]){
-                if(a[i] > b[i] && b[i] >= c[i] || a[i] > c[i] && c[i] >= b[i] || c[i] > a[i] && a[i] >= b[i] || c[i] > b[i] && b[i] >= a[i]){
+                if(a[i] > b[i] && b[i] >= c[i] || a[i] > c[i] && c[i] >= b[i] || c[i] > a[i] && a[i] >= b[i] || c[i] > b[i] && b[i] >= a[i] || a[i] == b[i] && b[i] == c[i]){
+                    //System.out.println(3);
                     cnt++;
                 }
             }else if(c[i-1] > a[i-1] && a[i-1] >= b[i-1] || c[i-1] > b[i-1] && b[i-1] >= a[i-1]){
-                if(a[i] > b[i] && b[i] >= c[i] || a[i] > c[i] && c[i] >= b[i] || b[i] > a[i] && a[i] >= c[i] || b[i] > c[i] && c[i] >= a[i]){
+                if(a[i] > b[i] && b[i] >= c[i] || a[i] > c[i] && c[i] >= b[i] || b[i] > a[i] && a[i] >= c[i] || b[i] > c[i] && c[i] >= a[i] && a[i] == b[i] && b[i] == c[i]){
+                    //System.out.println(4);
                     cnt++;
                 }
             }
