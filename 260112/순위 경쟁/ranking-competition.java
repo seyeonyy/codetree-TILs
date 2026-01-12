e89fee692dbab3992f2af934c7d6caf693c404bb
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2026-01-12 23:20:43 +0900

    Title: 순위 경쟁, Time: 135ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260112/순위 경쟁/ranking-competition.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n+1];
+        int[] b = new int[n+1];
+        int[] c = new int[n+1];
+        a[0] = 0;
+        b[0] = 0;
+        c[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            char ca = sc.next().charAt(0);
+            int s = sc.nextInt();
+            if(ca == 'A'){
+                a[i] = a[i-1] + s;
+            }
+            if(ca == 'B'){
+                b[i] = b[i-1] + s;
+            }
+            if(ca == 'C'){
+                c[i] = c[i-1] + s;
+            }
+        }
+        int cnt = 0;
+        for(int i = 1; i <= n; i++){
+            if(a[i-1] == b[i-1] && b[i-1] == c[i-1]){
+                if(a[i] != b[i] || b[i] != c[i]){
+                    cnt++;
+                }
+            }else if(a[i-1] > b[i-1] && b[i-1] >= c[i-1] || a[i-1] > c[i-1] && c[i-1] >= b[i-1]){
+                if(b[i] > c[i] && c[i] >= a[i] || b[i] > a[i] && a[i] >= c[i] || c[i] > a[i] && a[i] >= b[i] || c[i] > b[i] && b[i] >= a[i]){
+                    cnt++;
+                }
+            }else if(b[i-1] > c[i-1] && c[i-1] >= a[i-1] || b[i-1] > a[i-1] && a[i-1] >= c[i-1]){
+                if(a[i] > b[i] && b[i] >= c[i] || a[i] > c[i] && c[i] >= b[i] || c[i] > a[i] && a[i] >= b[i] || c[i] > b[i] && b[i] >= a[i]){
+                    cnt++;
+                }
+            }else if(c[i-1] > a[i-1] && a[i-1] >= b[i-1] || c[i-1] > b[i-1] && b[i-1] >= a[i-1]){
+                if(a[i] > b[i] && b[i] >= c[i] || a[i] > c[i] && c[i] >= b[i] || b[i] > a[i] && a[i] >= c[i] || b[i] > c[i] && c[i] >= a[i]){
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
